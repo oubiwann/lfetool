3ba9c1e552854a9e9f02092c3a7a6388694dc2ea
U-homepc1\ksj <ksj@homepc1.(none)>
2014-04-20 02:16:18 +0900

    Bourne shellized, tested in cygwin

--- plugins/yaws-bootstrap/filler.sh
+++ plugins/yaws-bootstrap/filler.sh
@@ -1,45 +1,45 @@
-fill-yaws-bootstrap-files () {
+fill_yaws_bootstrap_files () {
     # LFE modules
     template="plugins/yaws-bootstrap/templates/PROJECT-content.lfe.tmpl"
     pattern="{{YAWSBOOTSTRAPCONTENTMODULE}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
     template="plugins/yaws-bootstrap/templates/PROJECT-nav.lfe.tmpl"
     pattern="{{YAWSBOOTSTRAPNAVMODULE}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
     template="plugins/yaws-bootstrap/templates/PROJECT-routes.lfe.tmpl"
     pattern="{{YAWSBOOTSTRAPROUTESMODULE}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
     # css files
     template="plugins/yaws-bootstrap/templates/bootstrap.css"
     pattern="{{YAWSBOOTSTRAPCSS}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
     template="plugins/yaws-bootstrap/templates/bootstrap-min.css"
     pattern="{{YAWSBOOTSTRAPCSSMIN}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
     template="plugins/yaws-bootstrap/templates/bootstrap-slate.css"
     pattern="{{YAWSBOOTSTRAPCSSSLATE}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
     template="plugins/yaws-bootstrap/templates/bootstrap-slate-min.css"
     pattern="{{YAWSBOOTSTRAPCSSMINSLATE}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
     # js files
     template="plugins/yaws-bootstrap/templates/bootstrap.js"
     pattern="{{YAWSBOOTSTRAPJS}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
     template="plugins/yaws-bootstrap/templates/bootstrap-min.js"
     pattern="{{YAWSBOOTSTRAPJSMIN}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
     # font files
     template="plugins/yaws-bootstrap/templates/glyphicons-halflings-regular.eot"
     pattern="{{YAWSBOOTSTRAPFONTEOT}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
     template="plugins/yaws-bootstrap/templates/glyphicons-halflings-regular.svg"
     pattern="{{YAWSBOOTSTRAPFONTSVG}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
     template="plugins/yaws-bootstrap/templates/glyphicons-halflings-regular.ttf"
     pattern="{{YAWSBOOTSTRAPFONTTTF}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
     template="plugins/yaws-bootstrap/templates/glyphicons-halflings-regular.woff"
     pattern="{{YAWSBOOTSTRAPFONTWOFF}}"
-    fill-tool-var $template $pattern
+    fill_tool_var $template $pattern
 }
