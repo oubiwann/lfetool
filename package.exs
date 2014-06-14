89050443b9692eaffa1f6c70b9e50872c6ceecfe
Duncan McGreggor <duncan@cogitat.io>
2014-06-13 18:38:29 -0700

    Added LFE project files.

---
+++ package.exs
@@ -0,0 +1,7 @@
+Expm.Package.new(
+  name: "lfetool",
+  description: "lfetool",
+  version: "0.0.1",
+  keywords: ["LFE", "Lisp", "Library", "Utility", "Tool", "CLI", "Command Line"],
+  maintainers: [[name: "Duncan McGreggor", email: "oubiwann@lfe.io"]],
+  repositories: [[github: "lfe/lfetool"]])
