65eca7b668f0e683908273505edd3c8a41fcf884
Duncan McGreggor <duncan@cogitat.io>
2014-06-14 17:27:47 -0700

    Fixed formatting.

--- package.exs
+++ package.exs
@@ -2,6 +2,7 @@ Expm.Package.new(
   name: "lfetool",
   description: "lfetool",
   version: "2.0.0-dev",
-  keywords: ["LFE", "Lisp", "Library", "Utility", "Tool", "CLI", "Command Line"],
+  keywords: ["LFE", "Lisp", "Library", "Utility", "Tool", "CLI",
+             "Command Line"],
   maintainers: [[name: "Duncan McGreggor", email: "oubiwann@lfe.io"]],
   repositories: [[github: "lfe/lfetool"]])
