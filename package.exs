9a6ba9972694a1c00c12bde19874ad914fdbcf86
Duncan McGreggor <duncan@cogitat.io>
2014-06-14 17:25:56 -0700

    Updated to 2.0 dev version.

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(
   name: "lfetool",
   description: "lfetool",
-  version: "0.0.1",
+  version: "2.0.0-dev",
   keywords: ["LFE", "Lisp", "Library", "Utility", "Tool", "CLI", "Command Line"],
   maintainers: [[name: "Duncan McGreggor", email: "oubiwann@lfe.io"]],
   repositories: [[github: "lfe/lfetool"]])
