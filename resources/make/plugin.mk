8f3d7e85d753cc60c528ceeb859d15b504da430c
Duncan McGreggor <duncan@cogitat.io>
2014-06-21 21:09:30 -0700

    Updated to use new start func.

--- resources/make/plugin.mk
+++ resources/make/plugin.mk
@@ -1,5 +1,6 @@
 compile-plugins:
 	@echo "Compiling plugins ..."
 	@ERL_LIBS=$(ERL_LIBS) PATH=$(SCRIPT_PATH) erl \
+	-s lfetool \
 	-eval "'lfetool-plugin':'compile-plugins'('show-output')." \
 	-noshell -s erlang halt
